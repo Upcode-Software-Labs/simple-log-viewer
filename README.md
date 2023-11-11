342520bcabb5c2f2a3040b2ae78e5d96ff701269
SujithPS <sujithps534@gmail.com>
2023-11-11 21:21:55 +0530

    :art: Add version and publish package - Update readme with details of installation - Install and configure prettier

--- README.md
+++ README.md
@@ -1,15 +1,93 @@
-# Simplest Log Viewer UI
+# Minimalist Log Viewer UI
 
-A minimalist log viewer for reduced network footprint. 
+A minimalist log viewer for reduced network footprint.
 
-## How to run
+### Benefits
+
+- Easy to install
+- Simple UI to see desired logs
+- Minimal CPU/Memory footprints in the server
+- On-demand usage
+
+## How to run in local
 
 ### Set environment variables
+
 `export SIMPLE_LOG_UI_LOG_FILE_PATH=/tmp/logs/test.log`
+
 `export SIMPLE_LOG_UI_PORT=3000`
 
 ### Start the server
+
 `npm start`
 
 ### Access the UI
+
 Browse `http://localhost:3000/`
+
+## How to run in server
+
+### Set environment variables
+
+`export SIMPLE_LOG_UI_LOG_FILE_PATH=<path to read the file>`
+
+`export SIMPLE_LOG_UI_PORT=<port>`
+
+### Install the package
+
+`npm install -g minimalist-log-viewer`
+
+### Access the UI
+
+Browse `http://<server-ip>:<port>/` if you have exposed the port <port> to the world.
+Map the domain and do a proper reverse proxy configuration.
+
+- [Apache httpd](https://httpd.apache.org/docs/2.4/howto/reverse_proxy.html)
+- [Nginx](https://docs.nginx.com/nginx/admin-guide/web-server/reverse-proxy/)
+
+### How to run as a Daemon with PM2
+
+#### Install PM2
+
+- Follow [Install PM2](https://pm2.keymetrics.io/docs/usage/quick-start/) or else `npm install pm2@latest -g` in short.
+- Create a file `ecosystem.config.js` inside the server at a suitable location
+- Set the environmental varibales inside `ecosystem.config.js`
+
+  ```
+    module.exports = {
+        apps : [
+            {
+                name: "minimalist-log-viewer",
+                script: "minimalist-log-viewer",
+                watch: true,
+                env: {
+                    "SIMPLE_LOG_UI_LOG_FILE_PATH": "<Path to your file>",
+                    "SIMPLE_LOG_UI_PORT": <port>,
+                    }
+            }
+        ]
+    }
+  ```
+
+  Modify the values according to your need.
+
+  example:
+
+  ```
+   module.exports = {
+       apps : [
+           {
+               name: "minimalist-log-viewer",
+               script: "minimalist-log-viewer",
+               watch: true,
+               env: {
+                   "SIMPLE_LOG_UI_LOG_FILE_PATH": "/var/logs/tomcat/temp/spring.log",
+                   "SIMPLE_LOG_UI_PORT": 3000,
+                   }
+           }
+       ]
+   }
+  ```
+
+- `pm2 start minimalist-log-viewer --env ecosystem.config.js`
+- Verify with `pm2 info minimalist-log-viewer`
--- src/server.js
+++ src/server.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const fs = require('fs');
+#!/usr/bin/env node
 
-const app = express();
-const port = parseInt(process.env.SIMPLE_LOG_UI_PORT, 10) || 3000;
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const fs = require('fs')
 
-app.use(fileUpload());
+const app = express()
+const port = parseInt(process.env.SIMPLE_LOG_UI_PORT, 10) || 3000
+
+app.use(fileUpload())
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
 // Read the file path from an environment variable or use a default path
-const LOG_FILE_PATH = process.env.SIMPLE_LOG_UI_LOG_FILE_PATH || 'logs/test.log';
+const LOG_FILE_PATH = process.env.SIMPLE_LOG_UI_LOG_FILE_PATH || 'logs/test.log'
 
 app.get('/readlog', (req, res) => {
-    const linesToRead = parseInt(req.query.lines, 10) || 10; // Default to 10 lines if 'lines' query parameter is not provided
+  const linesToRead = parseInt(req.query.lines, 10) || 10 // Default to 10 lines if 'lines' query parameter is not provided
 
-    fs.readFile(LOG_FILE_PATH, 'utf-8', (err, logContent) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
+  fs.readFile(LOG_FILE_PATH, 'utf-8', (err, logContent) => {
+    if (err) {
+      return res.status(500).send(err)
+    }
 
-        // Process the log content and send the relevant data to the front end
-        const logData = processLogContent(logContent, linesToRead);
-        res.json({ logData });
-    });
-});
+    // Process the log content and send the relevant data to the front end
+    const logData = processLogContent(logContent, linesToRead)
+    res.json({ logData })
+  })
+})
 
 function processLogContent(logContent, linesToRead) {
-    // Split the log content into lines
-    const logLines = logContent.split('\n');
-    
-    // Get the last 'linesToRead' lines
-    const relevantLines = logLines.slice(-(linesToRead + 1)).reverse();
+  // Split the log content into lines
+  const logLines = logContent.split('\n')
 
-    return relevantLines;
+  // Get the last 'linesToRead' lines
+  const relevantLines = logLines.slice(-(linesToRead + 1)).reverse()
+
+  return relevantLines
 }
 
-
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
-
+  console.log(`Server is running at http://localhost:${port}`)
+})
